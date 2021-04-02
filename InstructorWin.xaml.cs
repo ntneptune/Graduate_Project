6c3db3c64196247f3645019213073920211bdb73
ntneptune <81236750+ntneptune@users.noreply.github.com>
2021-04-02 13:09:18 +0700

    Update InstructorWin.xaml.cs

--- InstructorWin.xaml.cs
+++ InstructorWin.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -85,19 +85,22 @@ namespace Interface_NAA
                         if (values[0] == Ele_Iso[0])
                         {
                             var ele_iso = Ele_Iso[1].Split(',');
-                            Ele_A = values[0];
-                            IsoNum = values[1];
-                            CrossSec = values[2];
-                            HF = values[5];
-                            Energy = values[6];
-                            txt.Write("Standard Isotope: " + Ele_Iso[0] +"-"+ ele_iso[0] + "\n");
-                            txt.Write(ele_iso[1] + "," + CrossSec + "," + HF + "," + Energy + "\n");
+                            if (ele_iso[0] == values[1])
+                            {
+                                Ele_A = values[0];
+                                IsoNum = values[1];
+                                CrossSec = values[2];
+                                HF = values[5];
+                                Energy = values[6];
+                                txt.Write("Standard Isotope: " + Ele_Iso[0] + "-" + ele_iso[0] + "\n");
+                                txt.Write(ele_iso[1] + "," + CrossSec + "," + HF + "," + Energy + "\n");
+                            }
                         }
                         for (int j = 0; j < i; j++)
                         {
                             string[] EleB_Mass = Convert.ToString(ListBox1.Items[j]).Split(',');
                             string[] ELE_B = EleB_Mass[0].Split('-');
-                            if (values[0] == ELE_B[0] && values[5] != "-")
+                            if (values[0] == ELE_B[0] && values[1] == ELE_B[1] && values[5] != "-")
                             {
                                 Element.Add(values[0]);
                                 Isotopenum.Add(values[1]);
@@ -121,7 +124,16 @@ namespace Interface_NAA
                 this.Close();
             }
         }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ListBox1.Items.Clear();
+        }
 
-       
+        private void Reverse_Click(object sender, RoutedEventArgs e)
+        {
+            int j = ListBox1.Items.Count;
+            ListBox1.Items.RemoveAt(j - 1);
+        }
+
     }
 }
