27fe62d6ca8043830c536a68469fc5f9c2bca6d9
ntneptune <81236750+ntneptune@users.noreply.github.com>
2021-04-07 10:37:24 +0700

    Add new Files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Dragablz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interface_NAA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<UserControl> ListUc = new List<UserControl>();
+        public MainWindow()
+        {
+            InitializeComponent();            
+            ListUc.Add(new UserControlInstructor());
+            ListUc.Add(new UserControlUser());
+           
+        }
+
+
+        private void ButtonPower_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridPanel_MouseDown(object sender, RoutedEventArgs e)
+        {
+            DragMove();
+        }
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock text = new TextBlock();
+            
+        }
+
+        private void tcMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int tabItem = ((sender as TabablzControl)).SelectedIndex;
+            if (e.Source is TabablzControl) 
+            {
+                switch (tabItem)
+                {
+                    case 0:    // HOME
+                        //MessageBox.Show("HOME");
+                        break;
+                    case 1:    //INSTRUCTOR
+                        //MessageBox.Show("INSTRUCTOR");
+                        InitializeInstructor();
+                        break;
+                    case 2:   // USER
+                        //MessageBox.Show("USER");
+                        InitializeUser();
+                        //UserWin objuser = new UserWin();
+                        //objuser.Show();
+                        
+                        break;
+                    case 3: //HELP
+                        //MessageBox.Show("HELP");
+                        break;
+                    case 4: //Github
+                        //MessageBox.Show("Github");
+                        break;
+                    default:
+                        MessageBox.Show("ERROR");
+                        break;
+                }
+            }
+
+        }
+
+        private void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+            if(screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+            }
+        }
+
+        private void InitializeInstructor()
+        {
+            //UserControl uc = new UserControlInstructor();
+            SwitchScreen(ListUc[0]);
+        }
+        private void InitializeUser()
+        {
+            //UserControl uc = new UserControlUser();
+            SwitchScreen(ListUc[1]);
+        }
+    }
+}
---
+++ ResultsGraph.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interface_NAA
+{
+    /// <summary>
+    /// Interaction logic for ResultsGraph.xaml
+    /// </summary>
+    public partial class ResultsGraph : Window
+    {
+
+        List<UserControl> ListUc = new List<UserControl>();
+        public ResultsGraph()
+        {
+                
+            InitializeComponent();
+            ListUc.Add(new UserControlGraph());
+            ListUc.Add(new UserControlDatagridIsotope());
+            ListUc.Add(new UserControlSolution());
+            //set Initialize to GRAPH
+            SwitchScreen(ListUc[0]);
+      
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
+            switch (index)
+            {
+                case 0: //GRAPH
+                    InitializeGraph();
+                    break;
+                case 1: //ISOTOPE DATA
+                    InitializeIsotopeGrid();
+                    break;
+                case 2: //SOLUTION
+                    InitializeSolution();
+                    break;
+                default:
+                    MessageBox.Show("Error");
+                    break;
+            }
+
+        }
+
+        private void ButtonPower_Click(object sender,RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+        private void GridPanelRG_MouseDown(object sender, RoutedEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+            if (screen != null)
+            {
+                StackPanelRG.Children.Clear();
+                StackPanelRG.Children.Add(screen);
+            }
+        }
+        private void InitializeGraph()
+        {
+            SwitchScreen(ListUc[0]);
+        }
+        private void InitializeIsotopeGrid()
+        {
+            SwitchScreen(ListUc[1]);
+
+        }
+        private void InitializeSolution()
+        {
+            SwitchScreen(ListUc[2]);
+        }
+ 
+
+   
+    }
+
+
+
+}
