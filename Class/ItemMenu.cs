324b2e9a7a84803acb59b55d0bd287fd64ad15dc
ntneptune <81236750+ntneptune@users.noreply.github.com>
2021-04-23 08:45:18 +0700

    Add files via upload

---
+++ Class/ItemMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+
+namespace Interface_NAA.Class
+{
+    public class ItemMenu
+    {
+        public ItemMenu(string header,List<SubItem> subItems)
+        {
+            Header = header;
+            SubItems = subItems;
+
+        }
+
+        public ItemMenu(string header,UserControl screen)
+        {
+            Header = header;
+            Screen = screen;
+
+        }
+        public string Header { get; set; }
+        public List<SubItem> SubItems { get; set; }
+        public UserControl Screen { get; set; }
+    }
+}
