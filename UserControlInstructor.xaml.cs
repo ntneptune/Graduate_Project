5f612335f72e9fa5ce7521268ce37b1f5330d0c0
ntneptune <81236750+ntneptune@users.noreply.github.com>
2021-04-23 09:00:09 +0700

    Update UserControlInstructor.xaml.cs

--- UserControlInstructor.xaml.cs
+++ UserControlInstructor.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Data.SqlClient;
 using System.IO;
 
 namespace Interface_NAA
@@ -26,102 +16,174 @@ namespace Interface_NAA
         public UserControlInstructor()
         {
             InitializeComponent();
+            //InitializeUploadInStructortxt();
+
+        }
+        
+        private void InitializeUploadInStructortxt()
+        {
+
+            string path = "Resources\\txtFile\\InstructorInputFile.txt";
+            string[] lines = System.IO.File.ReadAllLines(@path);
+            StreamWriter sw = new StreamWriter(@path);
+            foreach (string line in lines)
+            {
+                sw.WriteLine(line);
+            }   
+            sw.Close();
+
         }
 
-        private void addsam_Click(object sender, RoutedEventArgs e)
+        private void Addsam_Click(object sender, RoutedEventArgs e)
         {
-            ListBox1.Items.Add(samiso.Text);
-            samiso.Clear();
-            if (ListBox1.Items.Count == 5)
+            string TBsampletext = TB_sample.Text.Trim();
+            int checkError = 0;
+            try
             {
-                if (stdisotope.Text.Trim() == "")
-                {
-                    samiso.Visibility = Visibility.Hidden;
-                    MessageBox.Show("Please fill in all required data", "Error");
-
-                }
-                else
-                {
-                    string stdiSotope = stdisotope.Text.Trim();
-                    var stdiso = stdiSotope.Split(',');
-                    samiso.Visibility = Visibility.Hidden;
-                    this.Visibility = Visibility.Hidden;
-                }
+                string [] sampletext = TBsampletext.Split(',');
+                double mass = Convert.ToDouble(sampletext[1]);
             }
+            catch
+            {
+                checkError = -1;
+            }
+
+            if(checkError == 0)
+            {
+                ListBox1.Items.Add(TBsampletext);
+                TB_sample.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please fill the correct sample data.", "Error!!!", MessageBoxButton.OK);
+            }
+        
         }
 
-        public void addno_Click(object sender, RoutedEventArgs e)
+        public void Save_Click(object sender, RoutedEventArgs e)
         {
-            var stdiSotope = stdisotope.Text.Trim();
-            //string[] lines = sample.Text.Trim();
-            if (stdiSotope == "")
+            string TBstdtext = TB_stdisotope.Text.Trim();
+            string TBfluxtext = TB_Neutronflux.Text.Trim();
+            string Error = "";
+            int checkError = 0;
+            try
             {
-                MessageBox.Show("Please fill in all required data", "Error");
+                string[] stdtext = TBstdtext.Split(',');
+                double mass = Convert.ToDouble(stdtext[1]);
             }
-            else if (stdiSotope != "")
+            catch
             {
-                var Ele_Iso = stdiSotope.Split('-');
-                TextWriter txt = new StreamWriter("C:\\code\\C#\\Project\\Interface NAA\\Interface NAA\\txtFile" +
-                    "\\InstructorInputFile.txt");
-                txt.Write(Neutronflux.Text.Trim() + "\n");
-                string path = @"Isotope data.csv";
-                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                using (var input = new StreamReader(fs))
+                checkError = -1;
+                Error += "Please fill the correct std data." + Environment.NewLine;
+            }
+
+            try
+            {
+                string[] fluxtext = TBfluxtext.Split('x');
+                double number = Convert.ToDouble(fluxtext[0]);
+                string[] Epower = fluxtext[1].Split('^');
+                int power = Convert.ToInt32(Epower[1]);
+                int E = Convert.ToInt32(Epower[0]);
+                if (E != 10)
                 {
-                    input.ReadLine();
-                    string Ele_A, IsoNum, CrossSec, HF, Energy;
-                    List<string> Element = new List<string>();
-                    List<string> Isotopenum = new List<string>();
-                    List<double> CS = new List<double>();
-                    List<double> hf = new List<double>();
-                    List<double> E = new List<double>();
-                    List<double> Mass = new List<double>();
-                    while (!input.EndOfStream)
-                    {
-                        var line = input.ReadLine();
-                        var values = line.Split(',');
-                        int i = ListBox1.Items.Count;
-                        if (values[0] == Ele_Iso[0])
-                        {
-                            var ele_iso = Ele_Iso[1].Split(',');
-                            if (ele_iso[0] == values[1])
-                            {
-                                Ele_A = values[0];
-                                IsoNum = values[1];
-                                CrossSec = values[2];
-                                HF = values[5];
-                                Energy = values[6];
-                                txt.Write("Standard Isotope: " + Ele_Iso[0] + "-" + ele_iso[0] + "\n");
-                                txt.Write(ele_iso[1] + "," + CrossSec + "," + HF + "," + Energy + "\n");
-                            }
-                        }
-                        for (int j = 0; j < i; j++)
-                        {
-                            string[] EleB_Mass = Convert.ToString(ListBox1.Items[j]).Split(',');
-                            string[] ELE_B = EleB_Mass[0].Split('-');
-                            if (values[0] == ELE_B[0] && values[1] == ELE_B[1] && values[5] != "-")
-                            {
-                                Element.Add(values[0]);
-                                Isotopenum.Add(values[1]);
-                                CS.Add(Convert.ToDouble(values[2]));
-                                hf.Add(Convert.ToDouble(values[5]));
-                                E.Add(Convert.ToDouble(values[6]));
-                                Mass.Add(Convert.ToDouble(EleB_Mass[1]));
-                            }
-                        }
-                    }
-                    for (int o = 0; o < Element.Count(); o++)
-                    {
-                        txt.Write("Sample Isotope: " + Element[o] + "-" + Isotopenum[o] + "\n");
-                        txt.Write(Mass[o] + "," + CS[o] + "," + hf[o] + "," + E[o] + "\n");
-                    }
-                    //var a = Neutronflux.Text.Split('^');
-                    //neutronflux = Math.Pow(Convert.ToDouble(a[0]), Convert.ToDouble(a[1]));   //assume input data is correct
+                    throw new Exception();
                 }
-                txt.Close();
-                //this.Visibility = Visibility.Hidden;
-                //this.Close();
+               
+
             }
+            catch(Exception)
+            {
+                checkError = -1;
+                Error += "Please fill the correct neutorn flux data." + Environment.NewLine;
+            }
+            catch
+            {
+                checkError = -1;
+                Error += "Please fill the correct neutorn flux data." + Environment.NewLine;
+            }
+
+            try
+            {
+                if (ListBox1.Items.Count == 0)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch(ArgumentException)
+            {
+                checkError = -1;
+                Error += "Please fill in Listbox with sample data." + Environment.NewLine;
+            }
+
+            if (checkError == 0)
+            {
+ 
+                    var Ele_Iso = TBstdtext.Split('-');
+                    TextWriter txt = new StreamWriter("Resources\\txtFile\\InstructorInputFile.txt");
+                    txt.Write(TBfluxtext + "\n");
+                    string path = @"Isotope data.csv";
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    using (var input = new StreamReader(fs))
+                    {
+                        input.ReadLine();
+                        string Ele_A, IsoNum, CrossSec, HF, Energy;
+                        List<string> Element = new List<string>();
+                        List<string> Isotopenum = new List<string>();
+                        List<double> CS = new List<double>();
+                        List<double> hf = new List<double>();
+                        List<double> E = new List<double>();
+                        List<double> Mass = new List<double>();
+                        while (!input.EndOfStream)
+                        {
+                            var line = input.ReadLine();
+                            var values = line.Split(',');
+                            int i = ListBox1.Items.Count;
+                            if (values[0] == Ele_Iso[0])
+                            {
+                                var ele_iso = Ele_Iso[1].Split(',');
+                                if (ele_iso[0] == values[1])
+                                {
+                                    Ele_A = values[0];
+                                    IsoNum = values[1];
+                                    CrossSec = values[2];
+                                    HF = values[5];
+                                    Energy = values[6];
+                                    txt.Write("Standard Isotope: " + Ele_Iso[0] + "-" + ele_iso[0] + "\n");
+                                    txt.Write(ele_iso[1] + "," + CrossSec + "," + HF + "," + Energy + "\n");
+                                }
+                            }
+                            for (int j = 0; j < i; j++)
+                            {
+                                string[] EleB_Mass = Convert.ToString(ListBox1.Items[j]).Split(',');
+                                string[] ELE_B = EleB_Mass[0].Split('-');
+                                if (values[0] == ELE_B[0] && values[1] == ELE_B[1] && values[5] != "-")
+                                {
+                                    Element.Add(values[0]);
+                                    Isotopenum.Add(values[1]);
+                                    CS.Add(Convert.ToDouble(values[2]));
+                                    hf.Add(Convert.ToDouble(values[5]));
+                                    E.Add(Convert.ToDouble(values[6]));
+                                    Mass.Add(Convert.ToDouble(EleB_Mass[1]));
+                                }
+                            }
+                        }
+                        for (int o = 0; o < Element.Count(); o++)
+                        {
+                            txt.Write("Sample Isotope: " + Element[o] + "-" + Isotopenum[o] + "\n");
+                            txt.Write(Mass[o] + "," + CS[o] + "," + hf[o] + "," + E[o] + "\n");
+                        }
+
+                    }
+                    txt.Close();
+                    MainWindow.ListUc[2] = new UserControlUser();
+
+            }
+            else
+            {
+                MessageBox.Show(Error, "Error!!!", MessageBoxButton.OK);
+            }
+
+           
         }
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
@@ -131,13 +193,18 @@ namespace Interface_NAA
         private void Reverse_Click(object sender, RoutedEventArgs e)
         {
             int j = ListBox1.Items.Count;
-            //fix case count = 0 >> try exception
-            ListBox1.Items.RemoveAt(j - 1);
+            int index = j - 1;
+            if (index < 0)
+            {
+                index = 0;   
+            }
+            else
+            {
+                ListBox1.Items.RemoveAt(j - 1);
+            }         
+
         }
 
-        private void Neutronflux_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
+     
     }
 }
