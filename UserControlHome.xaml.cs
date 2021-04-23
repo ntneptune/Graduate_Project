6bbe29ef48b7f68c5b07c8f5ef8d1cbf329c0ada
ntneptune <81236750+ntneptune@users.noreply.github.com>
2021-04-23 08:58:47 +0700

    Add files via upload

---
+++ UserControlHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Interface_NAA
+{
+    /// <summary>
+    /// Interaction logic for UserControlHome.xaml
+    /// </summary>
+    public partial class UserControlHome : UserControl
+    {
+        public UserControlHome()
+        {
+            InitializeComponent();
+            ZoomViewbox.Width = 1024;
+            ZoomViewbox.Height = 2000;
+        }
+
+ 
+        private void UpdateViewBox(int newValue)
+        {
+            if ((ZoomViewbox.Width >= 0) && ZoomViewbox.Height >= 0)
+            {
+                ZoomViewbox.Width += newValue;
+                ZoomViewbox.Height += newValue;
+            }
+        }
+
+        private void UserControl_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.Modifiers != ModifierKeys.Control)
+                return;
+            UpdateViewBox((e.Delta > 0) ? 50 : -50);
+        }
+    }
+}
