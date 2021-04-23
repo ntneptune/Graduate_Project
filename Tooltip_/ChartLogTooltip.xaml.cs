551dbe3d3ddf0d5bdafe4a3168b11568eb69a369
ntneptune <81236750+ntneptune@users.noreply.github.com>
2021-04-23 08:46:11 +0700

    Add files via upload

---
+++ Tooltip_/ChartLogTooltip.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace Interface_NAA.Tooltip_
+{
+    /// <summary>
+    /// Interaction logic for ChartLogTooltip.xaml
+    /// </summary>
+    public partial class ChartLogTooltip : IChartTooltip
+    {
+        private TooltipData _data;
+        public ChartLogTooltip()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TooltipData Data
+        {
+            get { return _data; }
+            set
+            {
+                _data = value;
+                OnPropertyChanged("Data");
+            }
+        }
+
+        public TooltipSelectionMode? SelectionMode { get; set; }
+        public static object TooltipSelectionMode { get; internal set; }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
